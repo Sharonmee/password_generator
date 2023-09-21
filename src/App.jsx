3acaf1195c00d9659c430f27156b9466a8d1e2f7
Sharonmee <enamyawlui@gmail.com>
2023-09-21 09:57:37 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Faclipboard } from "react-icons/fa";
+
+function App(){
+    return (
+        <>
+            <div style={{backgroundColor:"blue",width:"100%",height:"100%"}}>
+            
+                {/* <form id="pg_form">
+                    <div className="result">
+                        <input type="text" id="result" placeholder="Min 6 char" readOnly />
+                        <div className="clipboard">
+                            <Faclipboard></Faclipboard>
+                        </div>
+                    </div>
+                </form> */}
+            
+                
+            </div>
+        </>
+    );
+}
+
+export default App;
